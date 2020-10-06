dd6acb6209b8aaa4259fa635793dc08f0785879e
pan-maciek <kozieja.maciej@gmail.com>
2020-10-07 00:51:45 +0200

    added Git-DotFiles, Get-DotFiles and Edit-DotFiles

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -2,10 +2,36 @@ Set-Theme robbyrussell
 Set-Prompt
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-function Get-Config-Files {
-    /usr/bin/git --git-dir=$HOME/git/dotfiles --work-tree=$HOME ls-tree -r master --name-only
+function Git-DotFiles {
+    /usr/bin/git --git-dir=$HOME/git/dotfiles --work-tree=$HOME @Args
 }
 
-function Edit-Config {
-    /usr/bin/git --git-dir=$HOME/git/dotfiles --work-tree=$HOME ls-tree -r master --name-only | fzf
+function Sync-DotFiles {
+    Git-DotFiles pull
+    Git-DotFiles push
+}
+
+function Get-DotFiles {
+    $Prev = Get-Location
+    Set-Location $HOME
+
+    $Files = Git-DotFiles ls-tree -r master --name-only
+
+    Set-Location $Prev
+    $Files
+}
+
+function Edit-DotFiles {
+    $Prev = Get-Location
+    Set-Location $HOME
+
+    $File = Get-DotFiles | Invoke-Fzf
+    if ([string]::IsNullOrEmpty($File)) {
+        Set-Location $Prev
+        return 
+    }
+    $File = [System.IO.Path]::Join($HOME, $File)
+    nvim ($File)
+
+    Set-Location $Prev
 }
